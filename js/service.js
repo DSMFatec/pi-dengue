21673baa1b195736bfa40589d3957beae4503b03
Giovani Alves <giovani.alves121@gmail.com>
2024-06-19 19:29:06 -0300

    feat: add debug

--- js/service.js
+++ js/service.js
@@ -1,8 +1,17 @@
 export const getInfoDengueData = async () => {
   const response = await fetch(
-    "https://info.dengue.mat.br/api/alertcity?geocode=3304557&disease=dengue&format=json&ew_start=1&ew_end=50&ey_start=2017&ey_end=2017"
+    "https://info.dengue.mat.br/api/alertcity?geocode=3304557&disease=dengue&format=json&ew_start=1&ew_end=50&ey_start=2017&ey_end=2017",
+    {
+      method: "GET",
+      mode: "no-cors",
+    }
   );
+
+  console.log('RESPONSE', response);
+
   const data = await response.json();
+
+  console.log('DATA', data);
 
   return data;
 };
