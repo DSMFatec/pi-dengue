2403337ff2832628240070a28e4129882cb68fdf
Giovani Alves <giovani.alves121@gmail.com>
2024-06-20 05:45:26 -0300

    feat: add chart integration

--- js/service.js
+++ js/service.js
@@ -1,17 +1,7 @@
 export const getInfoDengueData = async () => {
-  const response = await fetch(
-    "https://info.dengue.mat.br/api/alertcity?geocode=3304557&disease=dengue&format=json&ew_start=1&ew_end=50&ey_start=2017&ey_end=2017",
-    {
-      method: "GET",
-      mode: "no-cors",
-    }
-  );
-
-  console.log('RESPONSE', response);
+  const response = await fetch("https://api-dengue-pi.onrender.com/");
 
   const data = await response.json();
-
-  console.log('DATA', data);
 
   return data;
 };
